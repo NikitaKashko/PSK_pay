ad84babf1e4081d9c9f543ab1ed7ce35b7748b00
Nikita Kashko <kashkonikev@gmail.com>
2024-12-13 01:23:51 +0300

    Add admin zone

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bill, Profile, Meter, CreditCard
 
-# Register your models here.
+admin.site.register(Bill)
+admin.site.register(Profile)
+admin.site.register(Meter)
+admin.site.register(CreditCard)
